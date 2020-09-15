04e2fe64aa3dc60da9a1ecb9977326ef01a9604c
achew8 <70230739+achew8@users.noreply.github.com>
2020-09-14 23:23:16 -0700

    Update integral.py

--- integral.py
+++ integral.py
@@ -1,6 +1,7 @@
 from math import exp
 def f(x):
     return x*exp(-x)
+
 # Numerically Integrate f(x) from 0 to 50
 intgrl=0.0
 x=0
@@ -10,5 +11,3 @@ while x<xmax:
     intgrl+=dx*f(x)
     x+=dx
 print("Integral=",intgrl,"error=",abs(1-intgrl))
-     
-    
